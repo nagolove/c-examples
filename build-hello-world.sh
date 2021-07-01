cefec9d2a293e773040de97fc889889260c55224
nagolove <dekar.own@gmail.com>
2021-07-01 16:50:23 +0000

    more ignore.

--- build-hello-world.sh
+++ build-hello-world.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 EXECUTABLE="hello-world"
-gcc main.cpp -o $EXECUTABLE -O2 -fPIC -faggressive-loop-optimizations -Wall -g -std=c++17
+#gcc main.cpp -o $EXECUTABLE -O2 -fPIC -faggressive-loop-optimizations -Wall -g -std=c++17
+#gcc main-hello-world.cpp -o $EXECUTABLE -O0 -fPIC -Wall -g -std=c++17
+#gcc main-hello-world.cpp -o $EXECUTABLE -O0 -Wall -g -std=c++17
+#gcc main-hello-world.cpp -o $EXECUTABLE -O0 -g3 -gdwarf -Wall -g -std=c++17
+#gcc main-hello-world.cpp -o $EXECUTABLE -O0 -g3 -ggdb -gdwarf -Wall -g -std=c++17
+#gcc main-hello-world.cpp -o $EXECUTABLE -ggdb -Wall -g -std=c++17
+gcc main-hello-world.cpp -o $EXECUTABLE -ggdb -Wall -std=c++17
 strip $EXECUTABLE
