318d63494ed610c173bcef4f48b9b2a1f756d5be
nagolove <dekar.own@gmail.com>
2021-07-02 12:24:25 +0000

    reorg.

--- build-hello-world.sh
+++ build-hello-world.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-EXECUTABLE="hello-world"
+BIN_PATH="bin"
+EXECUTABLE=$BIN_PATH"/hello-world"
 #gcc main.cpp -o $EXECUTABLE -O2 -fPIC -faggressive-loop-optimizations -Wall -g -std=c++17
 #gcc main-hello-world.cpp -o $EXECUTABLE -O0 -fPIC -Wall -g -std=c++17
 #gcc main-hello-world.cpp -o $EXECUTABLE -O0 -Wall -g -std=c++17
 #gcc main-hello-world.cpp -o $EXECUTABLE -O0 -g3 -gdwarf -Wall -g -std=c++17
 #gcc main-hello-world.cpp -o $EXECUTABLE -O0 -g3 -ggdb -gdwarf -Wall -g -std=c++17
 #gcc main-hello-world.cpp -o $EXECUTABLE -ggdb -Wall -g -std=c++17
-gcc main-hello-world.cpp -o $EXECUTABLE -ggdb -Wall -std=c++17
+echo "Executable: $EXECUTABLE"
+gcc hello-world.cpp -o $EXECUTABLE -ggdb -Wall -std=c++17
 strip $EXECUTABLE
--- build-shared-lib.sh
+++ build-shared-lib.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-EXECUTABLE="a.out"
-gcc main.cpp -o $EXECUTABLE -O2 -fPIC -faggressive-loop-optimizations -Wall -g -std=c++17
+BIN_PATH="bin"
+EXECUTABLE=$BIN_PATH"/shared-1.so"
+echo "Executable: $EXECUTABLE"
+gcc shared-lib.cpp -o $EXECUTABLE -O2 -fPIC -faggressive-loop-optimizations -Wall -g -std=c++17
 strip $EXECUTABLE
